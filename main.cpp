2ee18e61d97c0bbfe06c1fc1243a466cd9eb9b8b
mutiara <mutiaratara30@gmail.com>
2026-06-08 20:57:36 +0700

    pointer

--- main.cpp
+++ main.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main() {
 
+void ubahNilai(Mahasiswa *mhs, float nilaiBaru) {
+    mhs->setNilai(nilaiBaru);
+}
+
+struct Stack {
+    string data[10];
+    int top = -1;
+
+    void push(string isi) {
+        top++;
+        data[top] = isi;
+    }
+
+    void tampil() {
+        cout << "\n=== Riwayat Aktivitas ===" << endl;
+        for (int i = top; i >= 0; i--) {
+            cout << data[i] << endl;
+        }
+    }
+};
     return 0;
 }
