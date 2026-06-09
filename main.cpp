f66610940760675fd8432ed708e9be85483585b3
mutiara <mutiaratara30@gmail.com>
2026-06-09 22:28:33 +0700

    pointer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-using namespace std;
+struct Transaksi {
+    string npm;
+    float nilaiLama, nilaiBaru;
+};
 
-int main() {
-
-    return 0;
+void ubahNilai(Mahasiswa *mhs, float nilaiBaru) {
+    mhs->setNilai(nilaiBaru);
 }
+
+Mahasiswa *p = &dataMhs[1];
+ubahNilai(p, 88);
+cout << "\nSetelah Nilai Mutiara Diubah" << endl;
+tampilData();
+
+Transaksi t1 = {"2507071001", 80, 88};
+cout << "\n Data Transaksi" << endl;
+cout << "NPM        : " << t1.npm << endl;
+cout << "Nilai Lama : " << t1.nilaiLama << endl;
+cout << "Nilai Baru : " << t1.nilaiBaru << endl;
