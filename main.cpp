b78b5ec7017b6ae7910b4e16d8e7a36d4dbced96
TAMAAM25 <tamamferdinan@gmail.com>
2026-06-09 22:23:49 +0700

    struct stack

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+struct Stack {
+    string data[10];
+    int top = -1;
+    void push(string isi) { data[++top] = isi; }
+    void tampil() {
+        cout << "\nRiwayat Aktivitas" << endl;
+        for (int i = top; i >= 0; i--) cout << data[i] << endl;
+    }
+};
 int main() {
+    
 
     return 0;
 }
