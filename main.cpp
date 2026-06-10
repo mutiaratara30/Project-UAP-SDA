e18a9850575915e4b197889f657793a1010016dc
TAMAAM25 <tamamferdinan@gmail.com>
2026-06-10 10:07:37 +0700

    update codingan

--- main.cpp
+++ main.cpp
@@ -12,6 +12,28 @@ struct Stack {
     }
 };
 
+Stack riwayat;
+riwayat.push("Input nilai Arifatul");
+riwayat.push("Input nilai Mutiara");
+riwayat.push("Ubah nilai Mutiara");
+riwayat.tampil();
+
+struct Queue {
+    string data[10];
+    int depan = 0, belakang = -1;
+    void enqueue(string isi) { data[++belakang] = isi; }
+    void tampil() {
+        cout << "\n Antrian Revisi Nilai" << endl;
+        for (int i = depan; i <= belakang; i++) cout << data[i] << endl;
+    }
+};
+
+Queue antrian;
+antrian.enqueue("Revisi nilai Mutiara");
+antrian.enqueue("Revisi nilai Tamam");
+antrian.enqueue("Revisi nilai Aldion");
+antrian.tampil();
+
 int main() {
     
 
